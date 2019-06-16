c54dbccb2f6e6cd9e9c72ee120b21be934308026
杜星 <duxing04@maoyan.com>
2019-06-16 11:20:03 +0800

    init project

---
+++ src/main/java/thinkinginjava/RandomTest.java
@@ -0,0 +1,20 @@
+package thinkinginjava;
+
+import java.util.Random;
+
+/**
+ * @author duxing
+ * @date 2019/06/16
+ */
+public class RandomTest {
+    //random类种子确定，随机数序列确定
+    public static void main(String[] args) {
+        Random random1 = new Random(100);
+        Random random2 = new Random(100);
+        for (int i = 0; i < 100; i++) {
+            System.out.println(String.format("random1:%s,random2:%s", random1.nextInt(), random2.nextInt()));
+            System.out.println(String.format("random1:%s,random2:%s", random1.nextInt(10), random2.nextInt(10)));
+            System.out.println(String.format("random1:%s,random2:%s", random1.nextInt(10), random2.nextInt(11)));
+        }
+    }
+}
